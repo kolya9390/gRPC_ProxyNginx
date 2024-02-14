bb365aa3e33f73fe443bc97a7b39fc4d79dfdf2e
kolya9390 <kolya9390@gmail.com>
2024-02-14 17:00:01 +0300

    proxy_nginx_replicated

--- geobackend2/client_geo2/servis/rpc_client/auth/interface.go
+++
@@ -1,8 +0,0 @@
-package auth_client
-
-import "context"
-
-type Auther interface {
-	Login(ctx context.Context,email,password string) (string,error)
-	Registeretion(ctx context.Context,name,email,password string)(int64,error)
-}
